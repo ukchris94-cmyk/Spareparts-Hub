1575f857e6ae615ec019cb119d7b8b4fbde7ee1d
Onisokien Ayonoadu <oni@pgrninc.com>
2026-07-01 07:14:37 -0600

    changes to fix privacy policy routing

--- app/frontend/src/main.jsx
+++ app/frontend/src/main.jsx
@@ -4,6 +4,12 @@ import App from "./App";
 import ErrorBoundary from "./components/ErrorBoundary";
 import "./index.css";
 
+const redirect = sessionStorage.getItem("redirect");
+if (redirect) {
+  sessionStorage.removeItem("redirect");
+  window.history.replaceState(null, "", redirect);
+}
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ErrorBoundary>
